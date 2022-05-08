c22ce7c6f3e3ae366cc6aede277a26b98d2f49a7
yao8762 <hot665992002@gmail.com>
2022-05-09 00:15:00 +0800

    filter messages with good

--- read_reviews.py
+++ read_reviews.py
@@ -1,23 +1,29 @@
+
 data = []
 count = 0
-
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+print(f'檔案讀取完畢，總共有{len(data)}筆資料')
 
 totle = 0
 for d in data:
 	totle += len(d)
-
-print(f'檔案讀取完畢，總共有{len(data)}筆資料, 留言的平均長度為: {totle/len(data)}')
+print(f'留言的平均長度為: {totle/len(data)}')
 
 new = []
-
 for d in data:
 	if len(d) < 100:
 		new.append(d)
-print(f'總共有 {len(new)} 比留言長度小於100')
-print(new[0])
+print(f'總共有 {len(new)} 筆留言長度小於100')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print(f'總共有 {len(good)} 筆留言有包含"good"')
+print(good[0])
+
