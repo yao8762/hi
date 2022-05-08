1796b20ac33321dcd74e373b19a9a58510b7e407
yao8762 <hot665992002@gmail.com>
2022-05-09 00:40:21 +0800

    revise list comprehension

--- read_reviews.py
+++ read_reviews.py
@@ -14,16 +14,19 @@ for d in data:
 	totle += len(d)
 print(f'留言的平均長度為: {totle/len(data)}')
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+new = [d for d in data if len(d) < 100]
 print(f'總共有 {len(new)} 筆留言長度小於100')
 
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
-print(f'總共有 {len(good)} 筆留言有包含"good"')
-print(good[0])
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+good = [d for d in data if 'good' in d]
+print(f'總共有 {len(good)} 筆留言提到"good"')
 
+bad = ['bad' in d for d in data]
+print(len(if bad is True))
